0b00f4dacd53dbd2c2b9d924f7cca01edf8dba27
gianlufiorini <gianlu.fior03@gmail.com>
2026-09-21 16:59:18 +0200

    Fixed issues in uvr versioning: some of the used packages have not been included

--- R-version/uvr-setup.R
+++ R-version/uvr-setup.R
@@ -5,4 +5,8 @@ uvr::r_install("4.6.1")
 uvr::r_pin("4.6.1")
 
 #Pinning R packages
-uvr::add(c("tidyverse", "gt", "skimr", "janitor", "naniar"))
+uvr::scan()
+uvr::add(c("tidyverse", "gt", "skimr", "janitor", "naniar", "mice", "ggmice", "pins", "uvr", "withr", "nbafrank/uvr-r"))
+
+#To align with the R and package versions used for the analyses one should use the function
+
